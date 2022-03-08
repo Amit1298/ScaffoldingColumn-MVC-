084e7e93dfeda49f451a985d2fbf3f629652e870
Amit <amitkr24031995@gmail.com>
2022-03-08 17:19:59 +0530

    Adding ScaffoldingColumn

---
+++ ScaffoldingColumn/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScaffoldingColumn.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string Age { get; set; }
+        [ScaffoldColumn(false)]
+        public string contact { get; set; }
+        [ScaffoldColumn(false)]
+        public string Address { get; set; }
+
+    }
+}
